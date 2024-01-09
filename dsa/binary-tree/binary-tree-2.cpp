0ae1df0e78ad75c7cc5eca9451d342c35b130b24
Somesh Ghosh <99559848+someshjoyguru@users.noreply.github.com>
2024-01-09 22:17:56 +0530

    binary-tree-2.cpp

--- dsa/binary-tree/binary-tree-2.cpp
+++ dsa/binary-tree/binary-tree-2.cpp
@@ -142,6 +142,83 @@ bool isIdentical(node * node1, node * node2) {
   return ((node1 -> data == node2 -> data) && isIdentical(node1 -> left, node2 -> left) && isIdentical(node1 -> right, node2 -> right));
 }
 
+vector<vector<int>> zigzagLevelOrder(node* root) {
+    vector<vector<int>> ans;
+    if (root==NULL) return ans;
+    bool l2r = false;
+    queue<node*> q;
+    q.push(root);
+    while(!q.empty()){
+        l2r = (!l2r);
+        int sz= q.size();
+        vector<int> v;
+        for (int i=0; i<sz; i++){
+            auto it = q.front();
+            q.pop();
+            if (it->left)q.push(it->left);
+            if (it->right)q.push(it->right);
+            v.push_back(it->data);
+        }
+        if (!l2r)reverse(v.begin(),v.end());
+        ans.push_back(v);
+    }
+    return ans;
+}
+
+// boundary traversal of a binary tree
+
+class boundaryTraversal{
+public:
+    bool isLeaf(node * root) {
+        return !root -> left && !root -> right;
+    }
+
+    void addLeftBoundary(node * root, vector < int > & res) {
+        node * cur = root -> left;
+        while (cur) {
+            if (!isLeaf(cur)) res.push_back(cur -> data);
+            if (cur -> left) cur = cur -> left;
+            else cur = cur -> right;
+        }
+    }
+    
+    void addRightBoundary(node * root, vector < int > & res) {
+        node * cur = root -> right;
+        vector < int > tmp;
+        while (cur) {
+            if (!isLeaf(cur)) tmp.push_back(cur -> data);
+            if (cur -> right) cur = cur -> right;
+            else cur = cur -> left;
+        }
+        for (int i = tmp.size() - 1; i >= 0; --i) {
+            res.push_back(tmp[i]);
+        }
+    }
+
+    void addLeaves(node * root, vector < int > & res) {
+        if (isLeaf(root)) {
+            res.push_back(root -> data);
+            return;
+        }
+        if (root -> left) addLeaves(root -> left, res);
+        if (root -> right) addLeaves(root -> right, res);
+    }
+
+    vector < int > printBoundary(node * root) {
+        vector < int > res;
+        if (!root) return res;
+
+        if (!isLeaf(root)) res.push_back(root -> data);
+
+        addLeftBoundary(root, res);
+
+        // add leaf nodes
+        addLeaves(root, res);
+
+        addRightBoundary(root, res);
+        return res;
+    }
+};
 
 int main(){
   return 0;
