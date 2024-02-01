8c1c10437297cb3a9aafc971cc1fa6e6c44d451a
Somesh Ghosh <99559848+someshjoyguru@users.noreply.github.com>
2024-02-01 22:35:49 +0530

    changed file structure

---
+++ D_Three_Activities.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  fastio()        ios::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL);
+#define ll long long
+
+void solve(){
+    
+}
+
+int main(){
+    fastio()
+    ll t=1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
